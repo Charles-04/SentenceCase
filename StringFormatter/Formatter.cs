1fb296c17e9894bac0f1a715fd996fa14c7f705d
Charles-04 <charlesmir04@gmail.com>
2023-01-15 19:06:10 +0100

    Add project files.

---
+++ StringFormatter/Formatter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace StringFormatter
+{
+    public static class Formatter
+    {
+        public static string ToSentenceCase(string Word)
+        {
+            try
+            {
+
+                var WordLength = Word.Trim().ToList().Count;
+                string NewWord = null;
+                if (WordLength > 1)
+                {
+                    var FirstCharacter = Word.Trim().Substring(0, 1).ToUpper(); 
+                    NewWord = FirstCharacter + Word.Substring(1, WordLength - 1).ToLower();
+
+                }
+                else
+                {
+                    Console.WriteLine("String length too short");
+                }
+                return NewWord;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using StringFormatter;
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Formatter.ToSentenceCase("dATtrwesW");
+            Console.WriteLine(word);
+        }
+    }
+}
